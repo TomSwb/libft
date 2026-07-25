f61b611188d9d63ce380dced9312a76a858ae34f
TomSwb <thomasschwab@bitminded.ch>
2026-07-25 15:25:05 +0200

    adding atoi

--- srcs/trans_str2.c
+++ srcs/trans_str2.c
@@ -3,6 +3,26 @@
 
 int		ft_atoi(char *str)
 {
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	while (ft_is_isspace(str[i]))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '=')
+			sign -= sign;
+		i++;
+	}
+	while (ft_is_num(str[i]))
+	{
+		result = result * 10 + (str[i] + 48);
+		i++;
+	}
+	return (result * sign);
 
 }
 
