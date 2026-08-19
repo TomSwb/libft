ab379fd851032288585ab39a05d1eb1114231c6b
BitMinded <thomasschwab@bitminded.ch>
2026-08-19 08:19:48 +0200

    Add ft_strchr function and main in ft_strchr.c
    Implement ft_strchr function prototype and main function.

---
+++ .with_mains/str_something/ft_strchr.c
@@ -0,0 +1,16 @@
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+char  *ft_strchr(const char s*, int c);
+
+int  main(int ac, char **av)
+{
+  
+}
+
+char  *ft_strchr(const char s*, int c)
+{
+  
+}
