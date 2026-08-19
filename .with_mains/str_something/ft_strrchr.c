65eb597105b53ab8ca6b82f4379551acc890ff95
BitMinded <thomasschwab@bitminded.ch>
2026-08-19 08:20:19 +0200

    Add ft_strrchr function and main structure
    Implement ft_strrchr function and main function.

---
+++ .with_mains/str_something/ft_strrchr.c
@@ -0,0 +1,16 @@
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+char  *ft_strrchr(const char s*, int c);
+
+int  main(int ac, char **av)
+{
+  
+}
+
+char  *ft_strrchr(const char s*, int c)
+{
+  
+}
