093eb2c2bcc6f4a050659aa98a98b894b090b49f
BitMinded <thomasschwab@bitminded.ch>
2026-08-19 08:17:13 +0200

    Add ft_strnstr.c with function prototypes
    Implement ft_strnstr function prototype and main function.

---
+++ .with_mains/str_something/ft_strnstr.c
@@ -0,0 +1,16 @@
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+char  *ft_strnstr(const char big*, const char little*, size_t len);
+
+int  main(int ac, char **av)
+{
+  
+}
+
+int  ft_strncmp(const char big*, const char little*, size_t len)
+{
+  
+}
