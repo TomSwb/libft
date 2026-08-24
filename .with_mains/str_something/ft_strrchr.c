64356da3b8798f126014341ecc124fbed58095c3
BitMinded <thomasschwab@bitminded.ch>
2026-08-24 11:37:18 +0200

    Update ft_strrchr.c

--- .with_mains/str_something/ft_strrchr.c
+++ .with_mains/str_something/ft_strrchr.c
@@ -1,5 +1,9 @@
+/**
+ * @author TomSwb
+ * @version 0.1
+ * @date 2026-08-21
+ */
 
-#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -7,10 +11,33 @@ char  *ft_strrchr(const char s*, int c);
 
 int  main(int ac, char **av)
 {
-  
+  char  *s;
+  int    c;
+
+  if (ac > 2)
+  {
+    s = av[1];
+    c = av[2][0];
+  }
+  else
+      (void) ac;
+  printf("FT: %s\n", ft_strrchr(s, c));
+  printf("OG: %s\n", strrchr(s, c));
 }
 
 char  *ft_strrchr(const char s*, int c)
 {
-  
+  char *ptr
+
+  ptr = NULL;
+  c = (char)c;
+  while (*s)
+  {
+    if (*s == c)
+      ptr = (char *)s;
+    s++;
+  }
+  if (c == '\0')
+    return ((char *)s);
+  return (ptr);
 }
