a43c7f463a7f4be6f8db70ce85ad7698b0029186
BitMinded <thomasschwab@bitminded.ch>
2026-08-25 11:26:11 +0200

    Add ft_bzero function implementation
    Implemented ft_bzero function to set memory to zero.

--- .with_mains/part_1/mem_something/ft_bzero.c
+++ .with_mains/part_1/mem_something/ft_bzero.c
@@ -1,3 +1,68 @@
+/**
+ * @author TomSwb
+ * @version 0.1
+ * @date 2026-08-25
+ */
 
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
-void	ft_bzero(void s[.n], size_t n);
+void	ft_bzero(void *b, size_t len);
+
+int  main(void)
+{
+  size_t  n = 5;
+  unsigned char  bufft[10] = {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'};
+  unsigned char  bufog[10] = {'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z', 'z'};
+  size_t  i = 0;
+  size_t  len = 10;
+
+  printf("Init FT buffer: ");
+  while (i < len)
+  {
+    printf("%zu == %02x / '%c', ", i, bufft[i], bufft[i]);
+    i++;
+  }
+  printf("\n");
+  i = 0;
+  printf("Init OG buffer: ");
+  while (i < len)
+  {
+    printf("%zu == %02x / '%c', ", i, bufog[i], bufog[i]);
+    i++;
+  }
+  printf("\n");
+  ft_bzero(bufft, n);
+  bzero(bufog, n);
+  i = 0;
+  printf("ft_bzero buffer: ");
+  while (i < len)
+  {
+    printf("%zu == %02x / '%c', ", i, bufft[i], bufft[i]);
+    i++;
+  }
+  printf("\n");
+  i = 0;
+  printf("bzero buffer: ");
+  while (i < len)
+  {
+    printf("%zu == %02x / '%c', ", i, bufog[i], bufog[i]);
+    i++;
+  }
+  printf("\n");
+}
+
+void  ft_bzero(void *b, size_t len)
+{
+  unsigned char  *ptr;
+  size_t  i;
+  
+  ptr = (unsigned char *)b;
+  i = 0;
+  while (i < len)
+  {
+    ptr[i] = '\0';
+    i++;
+  }
+}
