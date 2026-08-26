41275eccc7f314c5875424e7cea61bccbecdaf89
BitMinded <thomasschwab@bitminded.ch>
2026-08-26 11:40:02 +0200

    Update ft_memcmp.c

--- .with_mains/part_1/mem_something/ft_memcmp.c
+++ .with_mains/part_1/mem_something/ft_memcmp.c
@@ -1,1 +1,21 @@
-int		ft_memcmp(const void s1[.n], const void s2[.n], size_t n);
+/**
+ * @author TomSwb
+ * @version 0.1
+ * @date 2026-08-26
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+int	main(void)
+{
+	
+}
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	
+}
