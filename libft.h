4c98203b899302f88abf43781fb4c294739b6fde
BitMinded <thomasschwab@bitminded.ch>
2026-08-26 10:11:20 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -31,30 +31,31 @@
 /*****  Functions *****/
 
 /** Part 1 **/
+
+int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
-int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 
-int		ft_toupper(int c);
-int		ft_tolower(int c);
 int		ft_atoi(const char *nptr);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
-size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dsize);
+size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 // could not test og function strnstr
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 
-//TODO: void	*ft_memset(void s[.n], int c, size_t n);
-//TODO: void	ft_bzero(void s[.n], size_t n);
-//TODO: void	*ft_memcpy(void dest[.n], const void src(.n), size_t n);
-//TODO: void	*ft_memmove(void dest[.n], const void src[.n], size_t n);
-//TODO: void	*ft_memchr(const void s[.n], int c, size_t n);
-//TODO: int		ft_memcmp(const void s1[.n], const void s2[.n], size_t n);
+//TODO: void	ft_bzero(void *s, size_t n);
+//TODO: void	*ft_memchr(const void *s, int c, size_t n);
+//TODO: int		ft_memcmp(const void *s1, const void *s2, size_t n);
+//TODO: void	*ft_memcpy(void *dest, const void *src, size_t n);
+//TODO: void	*ft_memmove(void *dest, const void *src, size_t n);
+//TODO: void	*ft_memset(void *s, int c, size_t n);
 
 //TODO: void	*ft_calloc(size_t n, size_t size);
 //TODO: char	*ft_strdup(const char *s);
