b97a8232475b05ae02f13cbdc197f3c22f76626c
TomSwb <thomasschwab@bitminded.ch>
2026-08-28 21:59:12 +0200

    Updating readme

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: tomswb <tomswb@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/28 21:38:55 by tomswb            #+#    #+#             */
-/*   Updated: 2026/08/28 21:41:35 by tomswb           ###   ########.fr       */
+/*   Updated: 2026/08/28 21:59:00 by tomswb           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 /**
 * @brief 
-Fills the given array up to index 'len' with '\0' char
+Fills the given array up to index 'len' with '\0' char.
+
 * @param b 
 * @param len 
 */
