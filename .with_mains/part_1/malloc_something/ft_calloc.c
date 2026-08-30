641aa18428d33a746ac828fc246923056d2954a2
TomSwb <thomasschwab@bitminded.ch>
2026-08-30 20:22:12 +0200

    minor changes

--- .with_mains/part_1/malloc_something/ft_calloc.c
+++ .with_mains/part_1/malloc_something/ft_calloc.c
@@ -8,7 +8,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
 #include <stdio.h>
 
 void	*ft_calloc(size_t n, size_t size);
--- .with_mains/part_1/malloc_something/ft_strdup.c
+++ .with_mains/part_1/malloc_something/ft_strdup.c
@@ -1,1 +1,13 @@
+/**
+ * @author TomSwb
+ * @version 0.1
+ * @date 2026-08-30 
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 char	*ft_strdup(const char *s);
