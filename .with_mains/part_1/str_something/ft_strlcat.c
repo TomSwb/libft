e5e780d544bc7b0737878e9e194f0b371854f555
TomSwb <thomasschwab@bitminded.ch>
2026-09-02 12:49:44 +0200

    correcting strlcat with better var names

--- .with_mains/part_1/str_something/ft_strlcat.c
+++ .with_mains/part_1/str_something/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tomswb <tomswb@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 16:08:56 by tomswb            #+#    #+#             */
-/*   Updated: 2026/08/31 17:16:12 by tomswb           ###   ########.fr       */
+/*   Updated: 2026/09/02 12:49:00 by tomswb           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,25 @@ int	main(void)
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	j;
-	size_t	k;
+	size_t	i_dest;
+	size_t	i_src;
+	size_t	len_src;
 
-	j = 0;
-	while (src[j])
-		j++;
-	k = j;
-	i = 0;
-	while (i < size && dst[i])
-		i++;
-	if (i == size)
-		return (size + j);
-	j = 0;
-	while (j < (size - 1) - i && src[j])
+	i_src = 0;
+	while (src[i_src])
+		i_src++;
+	len_src = i_src;
+	i_dest = 0;
+	while (i_dest < size && dst[i_dest])
+		i_dest++;
+	if (i_dest == size)
+		return (size + i_src);
+	i_src = 0;
+	while (i_src < (size - 1) - i_dest && src[i_src])
 	{
-		dst[i + j] = src[j];
-		j++;
+		dst[i_dest + i_src] = src[i_src];
+		i_src++;
 	}
-	dst[i + j] = '\0';
-	return (i + k);
+	dst[i_dest + i_src] = '\0';
+	return (i_dest + len_src);
 }
