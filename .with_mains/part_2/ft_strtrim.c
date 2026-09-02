daa378cd3a5e1100bc48a270d58be88397e51067
BitMinded <thomasschwab@bitminded.ch>
2026-09-02 10:52:29 +0200

    Update ft_strtrim.c

--- .with_mains/part_2/ft_strtrim.c
+++ .with_mains/part_2/ft_strtrim.c
@@ -1,2 +1,22 @@
 
-char  *ft_strtrim(char const *s1, char const *set);
+#include <stdlib.h>
+#include <stdio.h>
+
+char	*ft_strtrim(char const *s1, char const *set);
+
+int	main(void)
+{
+	char const	s1[25] = "Hello World How are you?";
+	char const	set[5] = "eoau";
+	char	*result;
+
+	result = ft_strtrim(s1, set);
+	printf("s1 = %s, set = %s\n", s1, set);
+	printf("result = %s", result);
+	free(result);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	
+}
