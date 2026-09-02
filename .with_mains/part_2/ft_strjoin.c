edbd6b03b6e19a928693e76b2b60afc05e61d8c6
BitMinded <thomasschwab@bitminded.ch>
2026-09-02 10:32:32 +0200

    Update ft_strjoin.c

--- .with_mains/part_2/ft_strjoin.c
+++ .with_mains/part_2/ft_strjoin.c
@@ -3,19 +3,54 @@
 #include <stdio.h>
 
 char  *ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(char const *s);
 
 int	main(void)
 {
 	char const	s1[7] = "Hello ";
 	char const	s2[6] = "World";
+	char	*result;
 
+	result = ft_strjoin(s1, s2);
 	printf("s1 = %s, s2 = %s\n", s1, s2);
-	printf("result = %s", ft_strjoin(s1, s2));
+	printf("result = %s", result);
+	free(result);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
+	size_t	s_s1;
+	size_t	s_s2;
+	size_t i;
 
-	result = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	s_s1 = ft_strlen(s1);
+	s_s2 = ft_strlen(s2);
+	result = malloc(s_s1 + s_s2 + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		result[s_s1 + i] = s2[i];
+		i++;
+	}
+	result[s_s1 + i] = '\0';
+	return (result);
+}
+
+size_t	ft_strlen(char const *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
