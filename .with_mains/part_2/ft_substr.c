1a0f3ce06a3aec177a60947b9f6ebde9cc58b649
BitMinded <thomasschwab@bitminded.ch>
2026-09-02 09:27:11 +0200

    Add ft_substr function and test in main
    Implement ft_substr function to extract substring.

--- .with_mains/part_2/ft_substr.c
+++ .with_mains/part_2/ft_substr.c
@@ -1,2 +1,34 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+
 char  *ft_substr(char const *s, unsigned int start, size_t len);
+
+int	main(void)
+{
+	const char	s[] = "Hello World";
+	unsigned int	start = 6;
+	size_t	len = 5;
+
+	printf("s = %s\n", s);
+	printf("result = %s", ft_substr(s, start, len));
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*result;
+	size_t	i;
+
+	result = malloc(len + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i <= len)
+	{
+		result[i] = s[start];
+		start++;
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
