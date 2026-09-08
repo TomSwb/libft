7c455f0127db05bbef3e5719e5f787e3c5f16e9e
TomSwb <thomasschwab@bitminded.ch>
2026-09-08 13:27:07 +0200

    Complete ft_strtrim before testing

--- .with_mains/part_2/ft_strtrim.c
+++ .with_mains/part_2/ft_strtrim.c
@@ -1,13 +1,16 @@
 
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strchr(char const *s, int c);
+size_t	ft_strlen(char const *s);
 
 int	main(void)
 {
 	char const	s1[25] = "Hello World How are you?";
-	char const	set[5] = "eolHau";
+	char const	set[7] = "eolHau";
 	char	*result;
 
 	result = ft_strtrim(s1, set);
@@ -25,10 +28,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 	start = 0;
 	while (ft_strchr(set, s1[start]) != NULL)
 		start++;
-	end = ft_strlen(s1);
-	while (ft_strrchr(set, s1[end]) != NULL)
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[end]) != NULL && *set)
 			end--;
 	result = malloc(end - start + 1);
+	if (!result)
+		return (NULL);
 	while (start <= end)
 	{
 		result[start] = s1[start];
