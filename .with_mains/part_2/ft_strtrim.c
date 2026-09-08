84c86b5db88ed690a65deea0e0225927bc2faed3
BitMinded <thomasschwab@bitminded.ch>
2026-09-08 11:45:17 +0200

    Modify ft_strtrim to enhance trimming functionality
    Updated the set of characters for trimming and improved the ft_strtrim function to correctly allocate memory and copy trimmed characters.

--- .with_mains/part_2/ft_strtrim.c
+++ .with_mains/part_2/ft_strtrim.c
@@ -7,7 +7,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 int	main(void)
 {
 	char const	s1[25] = "Hello World How are you?";
-	char const	set[5] = "eoau";
+	char const	set[5] = "eolHau";
 	char	*result;
 
 	result = ft_strtrim(s1, set);
@@ -18,5 +18,22 @@ int	main(void)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	char	*result;
+	size_t	end;
+	size_t	start;
+
+	start = 0;
+	while (ft_strchr(set, s1[start]) != NULL)
+		start++;
+	end = ft_strlen(s1);
+	while (ft_strrchr(set, s1[end]) != NULL)
+			end--;
+	result = malloc(end - start + 1);
+	while (start <= end)
+	{
+		result[start] = s1[start];
+		start++;
+	}
+	result[start] = '\0';
+	return (result);
 }
