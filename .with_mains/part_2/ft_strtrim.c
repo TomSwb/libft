e7df59a37477f5f5c064fe6fe8f1d73b90de6744
BitMinded <thomasschwab@bitminded.ch>
2026-09-10 20:55:01 +0200

    Update ft_strtrim.c

--- .with_mains/part_2/ft_strtrim.c
+++ .with_mains/part_2/ft_strtrim.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set);
-char *ft_strdup(char const *s);
+char	*ft_strdup(char const *s);
 char	*ft_strchr(char const *s, int c);
 size_t	ft_strlen(char const *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
