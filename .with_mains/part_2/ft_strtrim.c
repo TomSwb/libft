b8b9656707857927ce52214dca66cd4246cfe75e
BitMinded <thomasschwab@bitminded.ch>
2026-09-10 11:37:17 +0200

    Update ft_strtrim.c

--- .with_mains/part_2/ft_strtrim.c
+++ .with_mains/part_2/ft_strtrim.c
@@ -25,13 +25,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 	size_t	start;
 
-	if (!s1)
+	if (!s1 || s1[0] == '\0')
 		return (NULL);
 	start = 0;
 	while (ft_strchr(set, s1[start]) != NULL && s1[start])
 		start++;
-	if (s1[start] == '\0')
-		return (NULL);
 	end = ft_strlen(s1) - 1;
 	while (ft_strchr(set, s1[end]) != NULL && end > 0)
 			end--;
