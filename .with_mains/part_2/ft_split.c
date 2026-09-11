23c9e51e953070687c1186b9c3c4f3d4fa26f7f3
BitMinded <thomasschwab@bitminded.ch>
2026-09-11 21:07:02 +0200

    Update ft_split.c

--- .with_mains/part_2/ft_split.c
+++ .with_mains/part_2/ft_split.c
@@ -37,5 +37,16 @@ int  main(void)
 
 char  **ft_split(char const *s, char c)
 {
-    ...
+    char **result;
+    size_t count;
+    size_t i;
+    
+    i = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+            count++;
+        i++;
+    }
+    
 }
