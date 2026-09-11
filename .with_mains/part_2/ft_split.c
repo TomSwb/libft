30749a7df39a4251390da07bcc8a0c2d8bfa63f3
BitMinded <thomasschwab@bitminded.ch>
2026-09-11 20:52:42 +0200

    Update ft_split.c

--- .with_mains/part_2/ft_split.c
+++ .with_mains/part_2/ft_split.c
@@ -1,2 +1,41 @@
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 char  **ft_split(char const *s, char c);
+
+int  main(void)
+{
+    char const s[] = "hello,world,!,how,you,doing";
+    char c = ",";
+    char **result;
+    int i;
+    int j;
+    
+    printf("og s = %s\n", s);
+    result = ft_split(s, c);
+    i = 0;
+    while (result[i] != NULL)
+    {
+        j = 0;
+        while (result[i][j])
+        {
+            write(1, &result[i][j], 1);
+            j++;
+        }
+        i++;
+    }
+    i = 0;
+    while (result[i] != NULL)
+    {
+        free(result[i]);
+        i++;
+    }
+    free(result);
+}
+
+char  **ft_split(char const *s, char c)
+{
+    ...
+}
