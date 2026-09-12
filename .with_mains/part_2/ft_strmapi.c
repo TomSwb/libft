10368169880a557c8da07f86e05afdfe52ee2b7d
BitMinded <thomasschwab@bitminded.ch>
2026-09-12 23:11:47 +0200

    Update ft_strmapi.c

--- .with_mains/part_2/ft_strmapi.c
+++ .with_mains/part_2/ft_strmapi.c
@@ -50,9 +50,7 @@ size_t ft_strlen(chat const *s)
 char ft_toupper(unsigned int i, char c)
 {
     (void)i;
-    char up_c;
-    
-    if (c >= 97 && c <= 122)
-        up_c = c + 32;
-    return (up_c);
+    if (c >= 'a' && c <= 'z')
+        return (c - 32);
+    return (c);
 }
