36f1fb6e8c791e28af7656be79c48e4e92cb6da7
BitMinded <thomasschwab@bitminded.ch>
2026-09-12 22:56:19 +0200

    Update ft_strmapi.c

--- .with_mains/part_2/ft_strmapi.c
+++ .with_mains/part_2/ft_strmapi.c
@@ -1,2 +1,14 @@
 
+#include <stdio.h>
+
 char  *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+int main(void)
+{
+    char const s[6] = "hello";
+    char const *result;
+    
+    printf("s = %s\n", s);
+    result = ft_strmapi(s, ft_capitlize());
+    printf("result = %s\n", result);
+}
