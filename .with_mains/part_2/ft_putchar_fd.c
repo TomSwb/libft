b51c6a4a71d29dcc524ae031c9140c13e07dc9c7
BitMinded <thomasschwab@bitminded.ch>
2026-09-12 21:41:06 +0200

    Update ft_putchar_fd.c

--- .with_mains/part_2/ft_putchar_fd.c
+++ .with_mains/part_2/ft_putchar_fd.c
@@ -1,2 +1,22 @@
 
+#include <unistd.h>
+#include <>
+
 void  ft_putchar_fd(char c, int fd);
+
+int main(int ac, char **av)
+{
+    int fd;
+    char c;
+    
+    if (ac != 2)
+        return (-1);
+    fd = open(av[1]);
+    c = 'c';
+    ft_putchar_fd(c, av[1]);
+}
+
+void  ft_putchar_fd(char c, int fd)
+{
+    write(fd, &c, 1);
+}
