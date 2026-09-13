cf6ddeff65fd207793553b4d7ed31c9024e585bf
BitMinded <thomasschwab@bitminded.ch>
2026-09-13 21:07:55 +0200

    Update ft_lstnew.c

--- .with_mains/part_3/ft_lstnew.c
+++ .with_mains/part_3/ft_lstnew.c
@@ -5,4 +5,14 @@ typedef struct	s_list
 	struct s_list	*next;
 }					t_list;
 
-t_list  *ft_lstnew(void  *content);
+t_list  *ft_lstnew(void  *content)
+{
+    t_list *node;
+    
+    node = malloc(sizeof(t_list));
+    if (!node)
+        return (NULL);
+    node->content = content;
+    node->next = NULL;
+    return (node);
+}
