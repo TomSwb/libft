3f54914a9eba68d491a97d502f2d060c9acc43b2
BitMinded <thomasschwab@bitminded.ch>
2026-09-13 21:27:32 +0200

    Update ft_lstnew.c

--- .with_mains/part_3/ft_lstnew.c
+++ .with_mains/part_3/ft_lstnew.c
@@ -1,9 +1,31 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef struct	s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+
+int main(void)
+{
+    int num = 42;
+    t_list *node;
+    
+    node = NULL;
+    if (node == NULL)
+        printf("node content is NULL\n");
+    node = ft_lstnew(&num);
+    if (node == NULL)
+        printf("node content is NULL\n");
+    else
+    {
+        printf("node has a t_list init\n");
+        printf("node content is %d", *(int*)node->content);
+        free(node);
+    }
+}
 
 t_list  *ft_lstnew(void  *content)
 {
