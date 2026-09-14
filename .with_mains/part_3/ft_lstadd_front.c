a5c60b0be18fa4b7c58a90564b89e7a7745b01dd
BitMinded <thomasschwab@bitminded.ch>
2026-09-14 10:55:42 +0200

    Update ft_lstadd_front.c

--- .with_mains/part_3/ft_lstadd_front.c
+++ .with_mains/part_3/ft_lstadd_front.c
@@ -6,3 +6,16 @@ typedef struct	s_list
 }					t_list;
 
 void  ft_lstadd_front(t_list **lst, t_list *new);
+
+int	main(void)
+{
+	
+}
+
+void  ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (new == NULL || lst == NULL)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
