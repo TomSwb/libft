c327926ebcb12a66194b8f543301609af9b08c14
BitMinded <thomasschwab@bitminded.ch>
2026-09-14 11:40:34 +0200

    Implement test for ft_lstadd_front function
    Added test code to demonstrate ft_lstadd_front functionality.

--- .with_mains/part_3/ft_lstadd_front.c
+++ .with_mains/part_3/ft_lstadd_front.c
@@ -1,4 +1,6 @@
 
+#include <stdio.h>
+
 typedef struct	s_list
 {
 	void			*content;
@@ -12,8 +14,18 @@ int	main(void)
 	t_list	*ptr;
 	t_list	new;
 	t_list	old;
+	int	n1 = 0;
+	int	n2 = 42;
 
+	old.content = &n1;
+	old.next = NULL;
+	ptr = &old;
+	new.content = &n2;
+	new.next = NULL;
+	printf("1st node content is: %d\n", *(int *)ptr->content);
 	ft_lstadd_front(&ptr, &new);
+	printf("1st node content is: %d\n", *(int *)ptr->content);
+	printf();
 }
 
 void  ft_lstadd_front(t_list **lst, t_list *new)
