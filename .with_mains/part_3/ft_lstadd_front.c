4eff42346809c89940bf51539fc87d771c74c45b
BitMinded <thomasschwab@bitminded.ch>
2026-09-15 11:31:38 +0200

    Update ft_lstadd_front.c

--- .with_mains/part_3/ft_lstadd_front.c
+++ .with_mains/part_3/ft_lstadd_front.c
@@ -23,9 +23,17 @@ int	main(void)
 	new.content = &n2;
 	new.next = NULL;
 	printf("1st node content is: %d\n", *(int *)ptr->content);
+	printf("1st node address is %p\n", (void *)ptr);
+	printf("Old node address is %p\n", (void *)&old);
+	printf("Old.next address is %p\n", (void *)old.next);
+	printf("New node address is %p\n", (void *)&new);
 	ft_lstadd_front(&ptr, &new);
 	printf("1st node content is: %d\n", *(int *)ptr->content);
-	printf();
+	printf("1st node address is %p\n", (void *)ptr);
+	printf("New node address is %p\n", (void *)&new);
+	printf("New.next node address is %p\n", (void *)ptr->next);
+	printf("Old node address is %p\n", (void *)&old);
+	printf("Old.next address is %p\n", (void *)old.next);
 }
 
 void  ft_lstadd_front(t_list **lst, t_list *new)
