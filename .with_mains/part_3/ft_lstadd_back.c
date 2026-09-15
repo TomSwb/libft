9df87b0982f120a7a0ebccc0beb419e54c3fbb58
BitMinded <thomasschwab@bitminded.ch>
2026-09-15 20:29:10 +0200

    Update ft_lstadd_back.c

--- .with_mains/part_3/ft_lstadd_back.c
+++ .with_mains/part_3/ft_lstadd_back.c
@@ -5,4 +5,11 @@ typedef struct	s_list
 	struct s_list	*next;
 }					t_list;
 
-void  ft_lstadd_back(t_list **lst, t_list *new);
+void  ft_lstadd_back(t_list **lst, t_list *new)
+{
+    if (!lst || !new)
+        return ;
+    while (*lst && *lst->next != NULL)
+        *lst = *lst->next;
+    lst.next = &new;
+}
