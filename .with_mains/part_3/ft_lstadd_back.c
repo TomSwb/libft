1991d7dcdebb7fdfbb11e45d696adb6898fd34bb
BitMinded <thomasschwab@bitminded.ch>
2026-09-15 21:06:51 +0200

    Update ft_lstadd_back.c

--- .with_mains/part_3/ft_lstadd_back.c
+++ .with_mains/part_3/ft_lstadd_back.c
@@ -1,9 +1,43 @@
 
+#include <stdio.h>
+
 typedef struct	s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+
+void  ft_lstadd_back(t_list **lst, t_list *new);
+
+int main(void)
+{
+    t_list *head;
+    t_list node1;
+    t_list node2;
+    t_list lastnode;
+    int n1 = 1;
+    int n2 = 2;
+    int last = 42;
+    
+    head = &node1;
+    node1.content = &n1;
+    node1.next = &node2;
+    node2.content = & n2;
+    node2.next = NULL;
+    lastnode.content = &last;
+    lastnode.next = NULL;
+    while (lst)
+    {
+        printf("lst address = %p\n", (void *)lst->next);
+        lst = lst->next;
+    }
+    ft_lstadd_back(&lst, &new);
+    while (lst)
+    {
+        printf("lst address = %p\n", (void *)lst->next);
+        lst = lst->next;
+    }
+}
 
 void  ft_lstadd_back(t_list **lst, t_list *new)
 {
