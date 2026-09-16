2587c40849ab75792e5bf885e898442b2a47e4b0
BitMinded <thomasschwab@bitminded.ch>
2026-09-16 20:43:38 +0200

    Update ft_lstclear.c

--- .with_mains/part_3/ft_lstclear.c
+++ .with_mains/part_3/ft_lstclear.c
@@ -1,9 +1,26 @@
 
+#include <unistd.h>
+#include <stdio.h>
+
 typedef struct	s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+
+void  ft_lstclear(t_list **lst, void (*del)(void*));
+void ft_delcontent(void *content);
+
+int main(void)
+{
+    t_list **lst;
+    t_list *node1;
+    t_list *node2;
+    t_list *node3;
+    
+    lst = malloc(sizeof(t_list *) * 4);
+    
+}
 
 void  ft_lstclear(t_list **lst, void (*del)(void*))
 {
@@ -18,4 +35,9 @@ void  ft_lstclear(t_list **lst, void (*del)(void*))
         free(*lst);
         *lst = temp;
     }
+}
+
+void ft_delcontent(void *content)
+{
+    free(content);
 }
