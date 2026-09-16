03be3df872a28cc8148d08e5356c97aa9a587783
BitMinded <thomasschwab@bitminded.ch>
2026-09-16 07:40:17 +0200

    Update ft_lstclear.c

--- .with_mains/part_3/ft_lstclear.c
+++ .with_mains/part_3/ft_lstclear.c
@@ -5,4 +5,11 @@ typedef struct	s_list
 	struct s_list	*next;
 }					t_list;
 
-void  ft_lstclear(t_list **lst, void (*del)(void*));
+void  ft_lstclear(t_list **lst, void (*del)(void*))
+{
+    t_list *ptr;
+    
+    ptr = *lst;
+    while (ptr && ptr->next != NULL)
+    {}
+}
