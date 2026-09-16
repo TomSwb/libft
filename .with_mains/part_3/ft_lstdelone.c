32937e0b76fd13c5bae292eda6eccb59b818f64b
BitMinded <thomasschwab@bitminded.ch>
2026-09-16 06:32:20 +0200

    Update ft_lstdelone.c

--- .with_mains/part_3/ft_lstdelone.c
+++ .with_mains/part_3/ft_lstdelone.c
@@ -11,6 +11,9 @@ typedef struct	s_list
 void  ft_lstdelone(t_list *lst, void (*del)(void*));
 void ft_delcontent(void *content);
 
+// Compile using fsanitizer=address or 
+// use with valgrind to test
+
 int main(void)
 {
     t_list *lst;
@@ -23,10 +26,6 @@ int main(void)
     lst->next = NULL;
     printf("address lst = %p\n", (void *)&lst);
     ft_lstdelone(lst, ft_delcontent);
-    if (lst == NULL)
-        printf("lst deleted successfully");
-    else
-        printf("lst still exist at %p", (void *)&lst);
 }
 
 void  ft_lstdelone(t_list *lst, void (*del)(void*))
