4096b203c6eb78a31ee14f19c949256a322e7c1b
BitMinded <thomasschwab@bitminded.ch>
2026-09-16 12:08:38 +0200

    Update ft_lstclear.c

--- .with_mains/part_3/ft_lstclear.c
+++ .with_mains/part_3/ft_lstclear.c
@@ -14,7 +14,7 @@ void  ft_lstclear(t_list **lst, void (*del)(void*))
     while (lst && ptr->next != NULL)
     {
         ptr = lst->next;
-        del(lst->content);
+        del(*lst->content);
         free(*lst);
         *lst = ptr;
     }
