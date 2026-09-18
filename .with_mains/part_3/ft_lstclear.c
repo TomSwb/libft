0f7b8c150f1ba385bdc46f5d14ae71c8dd6ce043
Thomas Schwab <thomasschwab@bitminded.ch>
2026-09-18 11:27:31 +0200

    Refactor ft_lstclear with enhanced memory management

--- .with_mains/part_3/ft_lstclear.c
+++ .with_mains/part_3/ft_lstclear.c
@@ -8,23 +8,75 @@ typedef struct	s_list
 	struct s_list	*next;
 }					t_list;
 
-void  ft_lstclear(t_list **lst, void (*del)(void*));
-void ft_delcontent(void *content);
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_delcontent(void *content);
 
 int main(void)
 {
-    t_list **lst;
-    t_list *node1;
-    t_list *node2;
-    t_list *node3;
+    t_list	**lst;
+	t_list	*temp;
+    t_list	*node1;
+    t_list	*node2;
+    t_list	*node3;
+	int		*n1;
+	int		*n2;
+	int		*n3;
+	size_t	i;
     
-    lst = malloc(sizeof(t_list *) * 4);
-    
+    node1 = malloc(sizeof(t_list));
+	node2 = malloc(sizeof(t_list));
+	node3 = malloc(sizeof(t_list));
+	n1 = malloc(sizeof(int));
+	n2 = malloc(sizeof(int));
+	n3 = malloc(sizeof(int));
+	*n1 = 40;
+	*n2 = 41;
+	*n3 = 42;
+	lst = &node1;
+	node1->content = n1;
+	node1->next = node2;
+	node2->content = n2;
+	node2->next = node3;
+	node3->content = n3;
+	node3->next = NULL;
+	temp = *lst;
+	i = 1;
+	while (temp)
+	{
+		printf("Node %zu address is: %p\n", i, (void)temp);
+		temp = temp->next;
+		i++;
+	}
+	temp = *lst;
+	i = 1;
+	lst = &(*lst)->next;
+	ft_lstclear(lst, ft_delcontent);
+	while (temp)
+	{
+		printf("Node %zu address is: %p\n", i, (void)temp);
+		temp = temp->next;
+		i++;
+	} 
+	if (*lst !=NULL)
+	{
+		printf("*lst not NULL, function failed");
+		return (-1);
+	}
+	ft_lstclear(&node1, ft_delcontent);
+	if (node1 != NULL || temp != NULL)
+	{
+		if (temp != NULL)
+			printf("Temp not NULL, function failed\n");
+		if (node1 != NULL)
+			printf("Node1 not NULL, function failed");
+		return (-1);
+	}
+	printf("Clearing was succesfull");	
 }
 
-void  ft_lstclear(t_list **lst, void (*del)(void*))
+void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-    t_list *temp;
+    t_list	*temp;
     
     if (!lst || !del)
         return ;
@@ -37,7 +89,7 @@ void  ft_lstclear(t_list **lst, void (*del)(void*))
     }
 }
 
-void ft_delcontent(void *content)
+void	ft_delcontent(void *content)
 {
     free(content);
 }
